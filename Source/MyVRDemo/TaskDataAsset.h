8544c446300d40330d85299825d6a980db9c708c
Kai Yan <kyan2@wpi.edu>
2021-03-09 22:35:50 -0500

    TaskDataAsset added.

---
+++ Source/MyVRDemo/TaskDataAsset.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "TaskDataAsset.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)
+enum class ETaskTypes : uint8 
+{
+	CreateMeshBased       UMETA(DisplayName = "CreateMeshBased"),
+	CreateRayMarching        UMETA(DisplayName = "CreateRayMarching"),
+	CreateRayMarchingOperation        UMETA(DisplayName = "CreateRayMarchingOperation"),
+	CreateBigFlocking        UMETA(DisplayName = "CreateBigFlocking"),
+	CreateSmallFlocking        UMETA(DisplayName = "CreateSmallFlocking"),
+	CreateAreaFog        UMETA(DisplayName = "CreateAreaFog"),
+	ChangeShapeMeshBased        UMETA(DisplayName = "ChangeShapeMeshBased"),
+	ChangeShapeRayMarching      UMETA(DisplayName = "ChangeShapeRayMarching"),
+	ChangeColorMeshBased      UMETA(DisplayName = "ChangeColorMeshBased"),
+	ChangeColorRayMarching      UMETA(DisplayName = "ChangeColorRayMarching"),
+	ScaleRayMarching      UMETA(DisplayName = "ScaleRayMarching"),
+	ScaleCube      UMETA(DisplayName = "ScaleCube"),
+	QueryResultUpdate            UMETA(DisplayName = "QueryResultUpdate"),
+	TagsUpdate              UMETA(DisplayName = "TagsUpdate")
+};
+
+USTRUCT(BlueprintType)
+struct FTaskStepData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		int32 Id;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FString Description;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FString Tips;
+	//
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		ETaskTypes Type;
+};
+
+
+USTRUCT(BlueprintType)
+struct FTaskData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		int32 Id;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FString Description;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TArray<FTaskStepData> TaskStepList;
+};
+
+UCLASS(BlueprintType)
+class MYVRDEMO_API UTaskDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		TArray<FTaskData> TaskDataList;
+};
